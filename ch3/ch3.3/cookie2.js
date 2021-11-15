ba0a2785bc64569cc769264b6da878aefae40709
BH-Kang <kabyho3513@gmail.com>
2021-11-16 00:31:02 +0900

    Update session

--- ch3/ch3.3/cookie2.js
+++ ch3/ch3.3/cookie2.js
@@ -6,12 +6,15 @@ const qs = require('querystring');
 const parseCookies = (cookie = '') =>
     // req.headers.cookie 문자열 객체로 바꾸어주는 함수
     cookie
-        .split(";")
+        .split(';')
         .map(v => v.split('='))
         .reduce((acc, [k, v]) => {
             acc[k.trim()] = decodeURIComponent(v);
             return acc;
         }, {});
+
+const session = {}; // 데이터 저장용
+// broser : 암호화 된 키를 보내 server의 중요 데이터 가져옴.
 
 http.createServer(async (req, res) => {
     const cookies = parseCookies(req.headers.cookie); // { mycookie: 'test' }
@@ -21,17 +24,22 @@ http.createServer(async (req, res) => {
         const { name } = qs.parse(query); // query string에서 name 추출
         const expires = new Date();
         expires.setMinutes(expires.getMinutes() + 5); // 5분 후 쿠키 만료 
+        const uniqueInt = Date.now(); // key 안 겹치게
+        session[uniqueInt] = {
+            name,
+            expires,
+        };
         res.writeHead(302, { // 302: redirection Location으로 돌려보내라.
             Location: '/', 
-            'Set-Cookie': `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            'Set-Cookie': `session=${uniqueInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
             // HttpOnly : javascript로 접근 못하게. 로그인에서 필수 (보안)
             // Path=/ : / 밑으로 cookie 전부 사용 가능
         });
         res.end();
-    } else if (cookies.name) {
-        // name이라는 쿠키가 존재 하는 경우
+    } else if (cookies.session && session[cookies.session].expires > new Date()) {
+        // 세션쿠키 존재 + 만료 기간 지나지 않은 경우
         res.writeHead(200, {'Content-Type' : 'text/plain; charset=utf-8' });
-        res.end(`${cookies.name}님 안녕하세요`);
+        res.end(`${session[cookies.session].name}님 안녕하세요`);
     } else {
         try { 
             const data = await fs.readFile('./cookie2.html');
@@ -45,4 +53,4 @@ http.createServer(async (req, res) => {
 })
     .listen(8084, () => {
         console.log('8084번 포트에서 대기 중');
-    })
+    });
