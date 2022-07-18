dd802ac07953c7e325684889e1d509efcc427245
BH-Kang <kabyho3513@gmail.com>
2022-07-19 01:26:45 +0900

    Buffer

--- ch2/buffer.js
+++ ch2/buffer.js
@@ -1,6 +1,6 @@
 const buffer = Buffer.from('저를 버퍼로 바꿔보세요');
 console.log('from():', buffer);
-console.log('length:', buffer.length);
+console.log('length:', buffer.length, 'bytes');
 console.log('toString():', buffer.toString());
 
 const array = [Buffer.from('띄엄 '), Buffer.from('띄엄 '), Buffer.from('띄어쓰기 ')];
