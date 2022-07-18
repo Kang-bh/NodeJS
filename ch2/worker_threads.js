84ff31e2558a100e283c392f4881d909f0527516
BH-Kang <kabyho3513@gmail.com>
2022-07-18 18:27:03 +0900

    노드 내장 모듈 : worker_thread

---
+++ ch2/worker_threads.js
@@ -0,0 +1,15 @@
+const { Worker, isMainThread, parentPort } = require('worker_threads');
+
+if (isMainThread) { // 메인 스레드
+    const threads = new Set();
+    const worker = new Worker(__filename);
+    worker.on('message', (value) => console.log('워커로부터', value));
+    worker.on('exit', ()=> console.log('워커 끝'));
+    worker.postMessage('ping');
+} else { // 워커 스레드
+ parentPort.on('message', (value) => { // 핑을 보냈던 것이 워커스레드에서 value로 들어온다.
+    console.log('부모로부터', value);
+    parentPort.postMessage('pong');
+    parentPort.close();
+ })
+}
